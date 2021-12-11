315c677985c224b316ba043048f0f94f682a37af
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-11 14:25:13 -0500

    derive PartialEq for jabberwocky

--- src/jabberwocky.rs
+++ src/jabberwocky.rs
@@ -1,9 +1,16 @@
 use std::fmt::{Display, Formatter};
- use std::ops::Add;
+use std::ops::Add;
 
+#[derive(PartialEq)]
 pub struct JabberWocky {
-  face: String,
-  body: char,
+    face: String,
+    body: char,
+}
+
+#[test]
+fn test_equal() {
+    assert!(JabberWocky::from(('🐸', '🐋')) == JabberWocky::from(('🐸', '🐋')));
+    assert!(JabberWocky::from(('🐸', '🐋')) != JabberWocky::from(('🧟', '🫀')));
 }
 
 impl Display for JabberWocky {
@@ -17,34 +24,49 @@ impl Add for JabberWocky {
 
     fn add(self, rhs: Self) -> Self::Output {
         let heads = self.face + &rhs.face;
-        Self { face: heads, body: self.body }
+        Self {
+            face: heads,
+            body: self.body,
+        }
     }
 }
 
 impl From<(char, char)> for JabberWocky {
-    fn from(tuple: (char, char)) -> Self { 
-        Self { face: tuple.0.to_string(), body: tuple.1 }
+    fn from(tuple: (char, char)) -> Self {
+        Self {
+            face: tuple.0.to_string(),
+            body: tuple.1,
+        }
     }
 }
 
 #[test]
 fn test_jabberwocky_from_tuple() {
-    let hollis: JabberWocky = ('👽','🦗').into();
+    let hollis: JabberWocky = ('👽', '🦗').into();
     assert_eq!(format!("{}", hollis), "👽-🦗=<");
 }
 
 #[test]
 fn test_jabberwocky_add() {
-  let harry = JabberWocky { face: '👹'.to_string(), body: '🦎'};
-  let frank = JabberWocky { face: '🦊'.to_string(), body: '🐋'};
+    let harry = JabberWocky {
+        face: '👹'.to_string(),
+        body: '🦎',
+    };
+    let frank = JabberWocky {
+        face: '🦊'.to_string(),
+        body: '🐋',
+    };
 
-  let double_jb = harry + frank;
+    let double_jb = harry + frank;
 
-  assert_eq!(format!("{}", double_jb), "👹🦊-🦎=<");
+    assert_eq!(format!("{}", double_jb), "👹🦊-🦎=<");
 }
 
 #[test]
 fn test_jabberwocky_display() {
-    let jb = JabberWocky { face: '👹'.to_string(), body: '🦎'};
+    let jb = JabberWocky {
+        face: '👹'.to_string(),
+        body: '🦎',
+    };
     assert_eq!(format!("{}", jb), "👹-🦎=<");
 }
